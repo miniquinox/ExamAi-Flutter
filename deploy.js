2e48dfa35fe83b5978647dffc9aeb995a7836d6a
Joaquin Carretero <quinocarreteromartinez@gmail.com>
2024-07-21 14:18:31 -0700

    update

--- deploy.js
+++ deploy.js
@@ -1,15 +1,11 @@
 const { exec } = require('child_process');
 
-console.log('NODE VERSION:', process.version);
-console.log('PATH:', process.env.PATH);
-console.log('GIT PATH:', '/opt/homebrew/bin/git');
-
 const command = 'gh-pages -d build/web';
 const options = {
   env: {
     ...process.env,
     PATH: '/opt/homebrew/bin:' + process.env.PATH,
-    GIT_EXEC_PATH: '/opt/homebrew/Cellar/git/2.45.2/libexec/git-core', // Ensure git executable path is set correctly
+    HOME: '/Users/quino' // Ensure HOME is set
   },
 };
 
