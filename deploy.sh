4513b5bfd720d9569c3c902240d4a4f3310e3998
Joaquin Carretero <quinocarreteromartinez@gmail.com>
2024-07-21 13:18:30 -0700

    debug

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
+
 export PATH="/opt/homebrew/bin:/opt/homebrew/Cellar/git/2.45.2/bin:$PATH"
 export GIT_EXEC_PATH="/opt/homebrew/Cellar/git/2.45.2/libexec/git-core"
 
 flutter clean
 rm -rf build
 flutter pub get
-flutter pub outdated
 
 echo "Building web..."
 flutter build web && echo 'examai.ai' > build/web/CNAME
